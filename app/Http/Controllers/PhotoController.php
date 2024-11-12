59f9b600eadc16c6dbeab41b12302a27e8e7e98d
Anna <anna_chekalova@ens.univ-artois.fr>
2024-11-12 11:12:47 +0100

    debut yesss

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PhotoController extends Controller
 {
-    //
+    function index (){
+        return view ("index");
+    }
+
+    function albums (){
+        return view ("albums");
+    }
 }
---
+++ resources/views/albums.blade.php
@@ -0,0 +1,4 @@
+@extends ("layout")
+@section ("contenu")
+le detail d'un album qui a l'id={{$id}}
+@endsection
