258523633b9b46368ed1751a0d8cceda7bea62c0
Maxime Delattre <maximedelattre@MacBook-Air-de-Maxime.local>
2024-11-19 10:29:20 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\PhotoController;
 
-Route::get('/', [AlbumController::class, 'index'])->name('accueil');
+Route::get('/', [PhotoController::class, 'index'])->name('accueil');
 
 // Routes pour les albums
 Route::get('/albums', [AlbumController::class, 'index'])->name('index');
