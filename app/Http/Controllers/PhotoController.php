68d917d58066f9cf786335d2abce745eb1049b3f
Maxime Delattre <maximedelattre@MacBook-Air-de-Maxime.local>
2024-11-19 10:27:39 +0100

    .

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -8,41 +8,11 @@ use App\Models\Album;
 
 class PhotoController extends Controller
 {
-    // Recherche de photos par mot-clé ou tag
-    public function search(Request $request)
-    {
-        $query = $request->input('query');
-        $photos = Photo::where('title', 'like', "%$query%")->get();
-
-        return view('photos.search', compact('photos'));
+    function index (){
+        return view ('index');
     }
 
-    // Affiche le formulaire pour ajouter une nouvelle photo
-    public function create()
-    {
-        $albums = Album::all(); // Pour afficher la liste des albums dans le formulaire
-        return view('photos.create', compact('albums'));
-    }
-
-    // Gère la soumission du formulaire pour ajouter une photo
-    public function store(Request $request)
-    {
-        $photo = new Photo();
-        $photo->title = $request->input('title');
-        $photo->album_id = $request->input('album_id');
-        $photo->url = $request->input('url'); // Pour l'ajout via une URL
-
-        $photo->save();
-
-        return redirect()->route('albums.show', $photo->album_id);
-    }
-
-    // Supprime une photo
-    public function destroy($id)
-    {
-        $photo = Photo::findOrFail($id);
-        $photo->delete();
-
-        return redirect()->back();
+    function albums (){
+        return view ('albums');
     }
 }
