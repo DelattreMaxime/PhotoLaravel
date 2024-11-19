76178054dfb35845a64d23232ecf1aee45e9ad53
Anna <anna_chekalova@ens.univ-artois.fr>
2024-11-19 10:12:57 +0100

    layout

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset='UTF-8'>
+	<title>Titre de la page</title>
+    <link rel= "stylesheet" 
+    type='text/css'
+    href = '{{env("APP_URL")}}/css/style.css' />
+
+	</head>
+
+	<body>
+	<nav>
+        <a href= "{{route('index')}}">home page </a>
+
+
+</nav>
+<main>
+    @yield ('contenu')
+</main>
+</body>
+</html>
