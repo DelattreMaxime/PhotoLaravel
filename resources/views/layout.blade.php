d413f6a3d507862f174dfd422f46ab85eb09d4fd
Maxime Delattre <maximedelattre@leni-147-18.irsf>
2024-11-26 15:18:14 +0100

    css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,21 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
-	<meta charset='UTF-8'>
-	<title>Titre de la page</title>
-    <link rel="stylesheet" 
-          type="text/css"
-          href="{{asset('app.css')}}" />
+    <meta charset='UTF-8'>
+    <title>Titre de la page</title>
+    <link rel="stylesheet" type="text/css" href="{{asset('app.css')}}" />
 </head>
 <body>
-        <nav class="navbar">
-            <a href="{{ route('accueil') }}" class="nav-link">Home Page</a>
-            <a href="{{ route('albums') }}" class="nav-link">Albums</a>
-            <form action="{{ route('photos.search') }}" method="GET">
-                <input type="text" name="query" placeholder="Rechercher une photo">
-                <button type="submit">Rechercher</button>
-            </form>
-        </nav>
+    <nav class="navbar">
+        <div class="nav-animation"></div>
+        <a href="{{ route('accueil') }}" class="nav-link nav-home">Home Page</a>
+        <a href="{{ route('albums') }}" class="nav-link nav-albums">Albums</a>
+        <form action="{{ route('photos.search') }}" method="GET" style="display: inline-block; margin-left: 20px;">
+            <input type="text" name="query" placeholder="Rechercher une photo">
+            <button type="submit">Rechercher</button>
+        </form>
+    </nav>
     <main>
         @yield('contenu')
     </main>
