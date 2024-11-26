08dad1e92c6900b33931639498bd067533f3c754
Maxime Delattre <maximedelattre@MacBook-Air-de-Maxime.local>
2024-11-26 10:55:12 +0100

    link css

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,19 +3,17 @@
 <head>
 	<meta charset='UTF-8'>
 	<title>Titre de la page</title>
-    <link rel= "stylesheet" 
-    type='text/css'
-    href = '{{env("APP_URL")}}/css/style.css' />
-
-	</head>
-
-	<body>
-	<nav>
-        <a href= "{{route('accueil')}}">home page </a>
-        <a href= "{{route('albums')}}">albums </a>
-</nav>
-<main>
-    @yield ('contenu')
-</main>
+    <link rel="stylesheet" 
+          type="text/css"
+          href="{{asset('app.css')}}" />
+</head>
+<body>
+    <nav class="navbar">
+        <a href="{{route('accueil')}}" class="nav-link">Home Page</a>
+        <a href="{{route('albums')}}" class="nav-link">Albums</a>
+    </nav>
+    <main>
+        @yield('contenu')
+    </main>
 </body>
 </html>
