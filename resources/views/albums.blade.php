03badcade9ae1381e6d611d082157e16f199c003
Maxime Delattre <maximedelattre@MacBook-Air-de-Maxime.local>
2024-12-03 11:20:35 +0100

    Trie des albums

--- resources/views/albums.blade.php
+++ resources/views/albums.blade.php
@@ -3,14 +3,16 @@
 @section ("contenu")
 
     <!-- Formulaire de tri -->
-    <form method="GET" action="{{ route('albums') }}">
-        <label for="sort">Trier par :</label>
-        <select name="sort" id="sort">
-            <option value="title">Titre</option>
-            <option value="date">Date</option>
-        </select>
-        <button type="submit">Appliquer</button>
-    </form>
+        <form method="GET" action="{{ route('albums') }}">
+            <label for="creation">Trier par :</label>
+            <select name="creation" id="creation" onchange="this.form.submit()">
+                <option value="title" {{ request('creation') == 'title' ? 'selected' : '' }}>Titre (A → Z)</option>
+                <option value="date_desc" {{ request('creation') == 'date_desc' ? 'selected' : '' }}>Date (récent → ancien)</option>
+                <option value="date_asc" {{ request('creation') == 'date_asc' ? 'selected' : '' }}>Date (ancien → récent)</option>
+            </select>
+        </form>
+
+
 
     <ul>
         @foreach ($albums as $album)
@@ -18,18 +20,19 @@
                 <a href="{{ route('photosAlbum', ['id' => $album->id]) }}">
                     {{ $album->titre }}
                 </a>
+                <span>{{$album->creation}}</span>
 
-                <!-- Dernière image ajoutée au hasard -->
+                <!-- image ajoutée au hasard -->
                 @php
                     $randomPhoto = DB::table('photos')
                         ->where('album_id', $album->id)
-                        ->inRandomOrder()  // Récupère une photo au hasard
+                        ->inRandomOrder()  
                         ->first();
                 @endphp
 
                 @if ($randomPhoto)
                     <div>
-                        <img src="{{ $randomPhoto->url }}" alt="Miniature de {{ $album->titre }}" style="width: 100px; height: 100px;">
+                        <img src="{{ $randomPhoto->url }}" alt="Miniature de {{ $album->titre }}" style="max-width: 15rem">
                     </div>
                 @else
                     <p>Aucune image disponible</p>
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use App\Http\Controllers\PhotoController;
 Route::get('/', [PhotoController::class, 'index'])->name('accueil');
 
 // Routes pour les albums
-Route::get('/albums', [AlbumController::class, 'albums'])->name('albums');
+Route::get('/albums', [AlbumController::class, 'index'])->name('albums');
 
 
-Route::get('/photosAlbum/{id}', [AlbumController::class, 'photosAlbum'])->name('photosAlbum');
+Route::get('/photosAlbum/{id}', [PhotoController::class, 'show'])->name('photosAlbum');
 
 Route::get('/search', [PhotoController::class, 'search'])->name('photos.search');
 
@@ -38,14 +38,3 @@ Route::delete('/photos/{photo}', [PhotoController::class, 'destroy'])->name('pho
 
 
 
-
-
-Route::get('/albums/create', [AlbumController::class, 'create']); // Formulaire création
-Route::post('/albums', [AlbumController::class, 'store']); // Ajouter un album
-Route::get('/albums/{id}', [AlbumController::class, 'show']); // Détails d'un album
-Route::delete('/albums/{id}', [AlbumController::class, 'destroy']); // Supprimer un album
-
-Route::get('/photos/{id}', [PhotoController::class, 'show']); // Afficher une photo
-Route::get('/photos/create', [PhotoController::class, 'create']); // Formulaire ajout photo
-Route::post('/photos', [PhotoController::class, 'store']); // Enregistrer une photo
-Route::delete('/photos/{id}', [PhotoController::class, 'destroy']); // Supprimer une photo
