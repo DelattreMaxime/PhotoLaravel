22af3bac691625a66909d44e890fa8de80857db2
DelattreMaxime <delattrem.pro@gmail.com>
2024-12-07 17:52:24 +0100

    css

--- resources/views/photos/create.blade.php
+++ resources/views/photos/create.blade.php
@@ -1,25 +1,25 @@
 @extends('layout')
 
 @section('contenu')
-    <h1>Ajouter une photo à l'album</h1>
+    <h1 class="page-title">Ajouter une photo à l'album</h1>
 
-    <form action="{{ route('photos.store', $albumId) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('photos.store', $albumId) }}" method="POST" enctype="multipart/form-data" class="form-container">
         @csrf
-        <div>
-            <label for="title">Titre :</label>
-            <input type="text" name="titre" id="titre" required>
+        <div class="form-group">
+            <label for="titre">Titre :</label>
+            <input type="text" name="titre" id="titre" required class="form-input">
         </div>
 
-        <div>
+        <div class="form-group">
             <label for="url">URL de la photo :</label>
-            <input type="url" name="url" id="url">
+            <input type="url" name="url" id="url" class="form-input">
         </div>
 
-        <div>
+        <div class="form-group">
             <label for="file">Ou téléversez une photo :</label>
-            <input type="file" name="file" id="file" accept="image/*">
+            <input type="file" name="file" id="file" accept="image/*" class="form-input">
         </div>
 
-        <button type="submit">Ajouter</button>
+        <button type="submit" class="submit-button">Ajouter</button>
     </form>
 @endsection
