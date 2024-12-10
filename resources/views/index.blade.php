2f116e843230f913f31f395b312fa1876253b839
Anna <anna_chekalova@ens.univ-artois.fr>
2024-12-10 11:17:51 +0100

    nav etc

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,8 @@
 @extends ("layout")
 @section ("contenu")
- <h1>Page d'accueil</h1>
- <img src="{{asset('logo/MMI ARCHIVE.png')}}">
+ <h1 class=bienvenue>Bienvenue sur</h1>
+ <img class= homelogo src="{{asset('logo/MMI ARCHIVE.png')}}">
+
+ <p class= texteaccueil> Plongez dans l’univers créatif des étudiants en Métiers du Multimédia et de l’Internet ! Ce site est une vitrine de projets novateurs, réalisés avec passion et savoir-faire, incluant des archives de travaux en design, développement, audiovisuel, et communication. Découvrez des portfolios variés, explorez des idées inspirantes, et suivez les dernières tendances digitales. Notre objectif ? Mettre en lumière le talent et la diversité des compétences qui façonnent l’avenir des médias numériques. Votre exploration commence ici ! </p>
 
 @endsection
